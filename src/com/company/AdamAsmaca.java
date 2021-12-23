7890b97abf44d19b77e6d306780605f1987302f4
blcouz <80945682+blcouz@users.noreply.github.com>
2021-12-23 19:33:30 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/com/company/AdamAsmaca.java

--- src/com/company/AdamAsmaca.java
+++ src/com/company/AdamAsmaca.java
@@ -124,6 +124,9 @@ public class AdamAsmaca {
                     AnaMenu();
                 }
             }
+            default -> {
+                AnaMenu();
+            }
         }
     }
 
